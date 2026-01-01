f5e01f97f50aa205e8641e1ca3765cae24f6df16
Simon Lydell <simon.lydell@gmail.com>
2026-01-01 14:05:30 +0100

    Add publish script

---
+++ publish.js
@@ -0,0 +1,25 @@
+import childProcess from "node:child_process";
+import fs from "node:fs";
+import path from "node:path";
+
+const VARIANTS = fs.readdirSync(
+  path.join(import.meta.dirname, "node_modules", "node-pty", "prebuilds")
+);
+
+for (const dir of [...VARIANTS, "top"]) {
+  console.info();
+  console.info(dir);
+
+  const publishResult = childProcess.spawnSync(
+    "npm",
+    ["publish", "--access=public"],
+    {
+      cwd: path.join(import.meta.dirname, "packages", dir),
+      shell: true,
+      stdio: "inherit",
+    }
+  );
+  if (publishResult.status !== 0) {
+    process.exit(publishResult.status);
+  }
+}
