4b92662ce5f1ad1cc4b4e34de61ce2ec9520e14e
Simon Lydell <simon.lydell@gmail.com>
2026-01-01 13:55:59 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,25 @@
+# @lydell/node-pty
+
+Smaller distribution of [microsoft/node-pty](https://github.com/microsoft/node-pty).
+
+- microsoft/node-pty ships with prebuilt binaries for multiple platforms. @lydell/node-pty only installs the prebuilt binaries needed for the current platform (depends on your package manager).
+- microsoft/node-pty supports compiling using node-gyp on unsupported platforms, which requires more source files. @lydell/node-pty only works on platforms with prebuilt binaries and never calls node-gyp.
+- microsoft/node-pty ships with unneeded files, such as source code TypeScript files, source maps and test files. @lydell/node-pty removes those.
+- microsoft/node-pty is about 60 MiB. @lydell/node-pty is less than 1 MiB on macOS and Linux, and around 30 MiB on Windows.
+
+@lydell/node-pty is built like this:
+
+1. Download the original node-pty npm package.
+2. Make one copy of it per supported platform.
+3. For each copy, only include relevant files.
+4. Make the @lydell/node-pty wrapper package, which depends on all the platform-specific packages using `"optionalDependencies"` in package.json, allowing package managers to only install the platform-specific package matching the current platform. The wrapper package only re-exports everything from the correct platform-specific package.
+
+## pty.js
+
+microsoft/node-pty is forked from [chjj/pty.js](https://github.com/chjj/pty.js) with the primary goals being to provide better support for later Node.js versions and Windows.
+
+## License
+
+Copyright (c) 2012-2015, Christopher Jeffrey (MIT License).<br>
+Copyright (c) 2016, Daniel Imms (MIT License).<br>
+Copyright (c) 2018, Microsoft Corporation (MIT License).
---
+++ build.js
@@ -0,0 +1,278 @@
+import fs from "node:fs";
+import path from "node:path";
+
+const VERSION = "TODO";
+
+const ORIGINAL_PACKAGE = path.join(
+  import.meta.dirname,
+  "node_modules",
+  "node-pty"
+);
+
+const PACKAGES = path.join(import.meta.dirname, "packages");
+
+const VARIANTS = [
+  {
+    platform: "darwin",
+    arch: "x64",
+  },
+  {
+    platform: "darwin",
+    arch: "arm64",
+  },
+  {
+    platform: "linux",
+    arch: "x64",
+  },
+  {
+    platform: "linux",
+    arch: "arm64",
+  },
+  {
+    platform: "win32",
+    arch: "x64",
+  },
+  {
+    platform: "win32",
+    arch: "arm64",
+  },
+];
+
+function buildTopPackage() {
+  const newPackage = path.join(PACKAGES, "top");
+
+  const packageJson = {
+    name: "@lydell/node-pty",
+    description: "Smaller distribution of node-pty.",
+    author: "Simon Lydell",
+    version: VERSION,
+    license: "MIT",
+    type: "commonjs",
+    exports: "./index.js",
+    types: "./node-pty.d.ts",
+    repository: {
+      type: "git",
+      url: "git://github.com/lydell/node-pty.git",
+    },
+    keywords: [
+      "pty",
+      "tty",
+      "terminal",
+      "pseudoterminal",
+      "forkpty",
+      "openpty",
+      "prebuild",
+      "prebuilt",
+    ],
+    optionalDependencies: Object.fromEntries(
+      VARIANTS.map(({ platform, arch }) => [
+        `@lydell/node-pty-${platform}-${arch}`,
+        VERSION,
+      ])
+    ),
+  };
+
+  const originalPackageJson = JSON.parse(
+    fs.readFileSync(path.join(import.meta.dirname, "package.json"), "utf-8")
+  );
+  const readme = fs.readFileSync(
+    path.join(import.meta.dirname, "README.md"),
+    "utf8"
+  );
+  const newReadme = `
+${readme.trim()}
+
+## Version
+
+@lydell/node-pty@${VERSION} is based on node-pty@${
+    originalPackageJson.dependencies["node-pty"]
+  }.
+
+## Prebuilt binaries
+
+This package includes prebuilt binaries for the following platforms and architectures:
+
+${VARIANTS.map(
+  ({ platform, arch }) =>
+    `- ${osName(platform)} ${archName(arch)} (${platform}-${arch})`
+).join("\n")}
+`.trim();
+
+  const types = fs
+    .readFileSync(
+      path.join(ORIGINAL_PACKAGE, "typings", "node-pty.d.ts"),
+      "utf-8"
+    )
+    .replace(`declare module 'node-pty'`, `declare module '@lydell/node-pty'`);
+
+  fs.mkdirSync(newPackage);
+
+  fs.writeFileSync(
+    path.join(newPackage, "package.json"),
+    JSON.stringify(packageJson, null, 2)
+  );
+
+  fs.writeFileSync(path.join(newPackage, "README.md"), newReadme);
+
+  fs.writeFileSync(path.join(newPackage, "node-pty.d.ts"), types);
+
+  fs.cpSync(
+    path.join(import.meta.dirname, "index.js"),
+    path.join(newPackage, "index.js")
+  );
+
+  fs.cpSync(
+    path.join(import.meta.dirname, "LICENSE"),
+    path.join(newPackage, "LICENSE")
+  );
+}
+
+function osName(platform) {
+  switch (platform) {
+    case "darwin":
+      return "macOS";
+    case "linux":
+      return "Linux";
+    case "win32":
+      return "Windows";
+    default:
+      return platform;
+  }
+}
+
+function archName(arch) {
+  switch (arch) {
+    case "x64":
+      return "x86_64";
+    case "arm64":
+      return "ARM64";
+    default:
+      return arch;
+  }
+}
+
+function buildSubPackage({ platform, arch }) {
+  const name = `${platform}-${arch}`;
+  const platformType = platform === "win32" ? "windows" : "unix";
+  const newPackage = path.join(PACKAGES, name);
+
+  fs.cpSync(ORIGINAL_PACKAGE, newPackage, {
+    recursive: true,
+    filter: (source) => {
+      const segments = path.relative(ORIGINAL_PACKAGE, source).split(path.sep);
+      const firstSegment = segments.at(0);
+      const secondSegment = segments.at(1);
+      const lastSegment = segments.at(-1);
+      switch (firstSegment) {
+        case "":
+        case "LICENSE":
+          return true;
+
+        case "prebuilds":
+          return secondSegment === undefined || secondSegment === name;
+
+        case "lib":
+          const type =
+            lastSegment.includes("windows") ||
+            lastSegment.includes("conpty") ||
+            lastSegment.includes("conout")
+              ? "windows"
+              : lastSegment.includes("unix")
+              ? "unix"
+              : "shared";
+          return (
+            !lastSegment.includes(".") ||
+            (lastSegment.endsWith(".js") &&
+              !lastSegment.endsWith(".test.js") &&
+              (type === "shared" || type === platformType))
+          );
+
+        case "binding.gyp":
+        case "deps":
+        case "package.json":
+        case "README.md":
+        case "scripts":
+        case "src":
+        case "third_party":
+        case "typings":
+          return false;
+
+        default:
+          throw new Error(
+            `Need to decide if should be included: ${firstSegment}`
+          );
+      }
+    },
+  });
+
+  fix(newPackage);
+
+  const packageJson = {
+    name: `@lydell/node-pty-${platform}-${arch}`,
+    version: VERSION,
+    description: `The node-pty package, stripped down for only ${platform}-${arch}.`,
+    repository: {
+      type: "git",
+      url: "git://github.com/lydell/node-pty.git",
+    },
+    license: "MIT",
+    type: "commonjs",
+    exports: "./lib/index.js",
+    os: [platform],
+    cpu: [arch],
+  };
+
+  const readme = `
+# ${packageJson.name}
+
+${packageJson.description}
+`.trim();
+
+  fs.writeFileSync(
+    path.join(newPackage, "package.json"),
+    JSON.stringify(packageJson, null, 2)
+  );
+
+  fs.writeFileSync(path.join(newPackage, "README.md"), readme);
+}
+
+// Make sure files are executable, and remove empty directories.
+function fix(dir) {
+  const entries = fs.readdirSync(dir, { withFileTypes: true });
+  let numEntries = entries.length;
+  for (const entry of entries) {
+    const entryPath = path.join(dir, entry.name);
+    if (entry.isFile()) {
+      if (
+        entryPath.endsWith(".node") ||
+        entryPath.endsWith(".pdb") ||
+        entryPath.endsWith("spawn-helper")
+      ) {
+        // At least on macOS, the files need to be executable.
+        fs.chmodSync(entryPath, 0o755);
+      }
+    } else if (entry.isDirectory()) {
+      const numChildEntries = fix(entryPath);
+      if (numChildEntries === 0) {
+        fs.rmdirSync(entryPath);
+        numEntries--;
+      }
+    } else {
+      throw new Error(`Neither a file nor directory: ${entryPath}`);
+    }
+  }
+  return numEntries;
+}
+
+function run() {
+  fs.rmSync(PACKAGES, { recursive: true, force: true });
+  fs.mkdirSync(PACKAGES);
+
+  buildTopPackage();
+
+  for (const variant of VARIANTS) {
+    buildSubPackage(variant);
+  }
+}
+
+run();
