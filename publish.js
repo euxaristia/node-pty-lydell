a4cdf4f25829248376992bb37912c0bbd7734e40
Simon Lydell <simon.lydell@gmail.com>
2026-01-04 17:28:45 +0100

    Add `--tag` argument to `npm publish` command

--- publish.js
+++ publish.js
@@ -2,6 +2,20 @@ import childProcess from "node:child_process";
 import fs from "node:fs";
 import path from "node:path";
 
+const [, , topTag, ...restArgs] = process.argv;
+
+if (topTag === undefined) {
+  console.error(
+    "You must pass the wanted tag name for the top npm package as the only argument."
+  );
+  process.exit(1);
+}
+
+if (restArgs.length !== 0) {
+  console.error(`Expected exactly one argument, but got: ${restArgs.length}`);
+  process.exit(1);
+}
+
 const VARIANTS = fs.readdirSync(
   path.join(import.meta.dirname, "node_modules", "node-pty", "prebuilds")
 );
@@ -10,9 +24,10 @@ for (const dir of [...VARIANTS, "top"]) {
   console.info();
   console.info(dir);
 
+  const tag = dir === "top" ? topTag : "latest";
   const publishResult = childProcess.spawnSync(
     "npm",
-    ["publish", "--access=public"],
+    ["publish", "--access=public", `--tag=${tag}`],
     {
       cwd: path.join(import.meta.dirname, "packages", dir),
       stdio: "inherit",
